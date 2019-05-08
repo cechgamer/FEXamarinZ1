d67ce63ac966fa55d08de50b0e978f1cf650204c
cech.castellon <47920140+cechgamer@users.noreply.github.com>
2019-05-08 19:04:52 -0400

    Xamarin 3

--- XamarinZ/App/App/App.xaml.cs
+++ XamarinZ/App/App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace App
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
